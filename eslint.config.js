8f757f454af477611c20443bfbe7b7b71ac6a8e4
aliyar <toktogulovaliyar24@gmail.com>
2026-09-19 17:17:18 +0500

    chore: configure TypeScript and ESLint

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    {
+        ignores: ['dist', 'node_modules'],
+    },
+
+    eslint.configs.recommended,
+
+    ...tseslint.configs.recommended,
+
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            globals: globals.browser,
+        },
+        linterOptions: {
+            noInlineConfig: true,
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'error',
+        },
+    },
+);
